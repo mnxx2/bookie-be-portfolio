d983bf430e4dcdb0f5d390429f040ad4b05181c1
mnxx2 <alsdk4931@gmail.com>
2025-10-13 21:10:51 +0900

    app.js에 sequelize import

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const express = require("express");
 const models = require("./models");
 const path = require("path");
+const { Sequelize } = require("sequelize");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const booksRouter = require("./routes/books");
